80ef07b813864f5308f85413f4629213c6657f7f
mbeka02 <antonymbeka@gmail.com>
2024-04-15 19:40:19 +0300

    feat: init auth page

---
+++ pacific/src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,62 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+export default function StudentSignUpPage() {
+  return (
+    <div className="">
+      <Card className="max-w-md">
+        <CardHeader>
+          <CardTitle className="text-xl">Create Student Account</CardTitle>
+          <CardDescription>
+            Enter all the details to create a student account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Enter the student email address"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Enter the full name"
+                required
+              />
+            </div>
+
+            <div className="grid gap-2">
+              <Label htmlFor="reg_no">Registration Number</Label>
+              <Input
+                id="reg_no"
+                type="text"
+                name="reg_no"
+                placeholder="Enter the registration number"
+                required
+              />
+            </div>
+            <Button type="submit" className="w-full">
+              Create an account
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
