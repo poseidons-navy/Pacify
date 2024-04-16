43b1a60146db912012fbc476554055a6ad6e8f08
mbeka02 <antonymbeka@gmail.com>
2024-04-16 12:02:16 +0300

    feat : added create admin fn

---
+++ pacific/src/validation/students.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+
+export const createStudentSchema = z.object({
+  email: z.string({ required_error: "Please input an email value." }).email(),
+  name: z.string().min(2).max(50),
+  registrationNumber: z
+    .string()
+    .min(10, { message: "Invalid registration number" }),
+  courseName: z.string(),
+  universityName: z.string(),
+});
