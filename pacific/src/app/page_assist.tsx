875c5fce471feb48d4ad32a8993e5ba5c65b9260
mbeka02 <antonymbeka@gmail.com>
2024-04-16 15:38:19 +0300

    feat : completed @txnlab/use-wallet integration

--- pacific/src/app/page_assist.tsx
+++ pacific/src/app/page_assist.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { useWallet } from "@txnlab/use-wallet";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 export default function PageAssist() {
   const { providers, activeAccount } = useWallet();
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       {providers?.map((provider) => (
         <div key={provider.metadata.id}>
-          <h4>
-            <img
+          <h4 className="flex font-semibold items-center py-4 gap-x-2">
+            <Image
               width={30}
               height={30}
               alt={`${provider.metadata.name} icon`}
@@ -16,43 +18,46 @@ export default function PageAssist() {
             {provider.metadata.name} {provider.isActive && "[active]"}
           </h4>
 
-          <div>
-            <button
+          <div className="gap-x-4 flex my-1">
+            <Button
+              size="sm"
               type="button"
               onClick={provider.connect}
               disabled={provider.isConnected}
             >
               Connect
-            </button>
-            <button
+            </Button>
+            <Button
+              size="sm"
               type="button"
               onClick={provider.disconnect}
               disabled={!provider.isConnected}
             >
               Disconnect
-            </button>
-            <button
+            </Button>
+            <Button
+              size="sm"
               type="button"
               onClick={provider.setActiveProvider}
               disabled={!provider.isConnected || provider.isActive}
             >
               Set Active
-            </button>
+            </Button>
 
             <div>
-                {provider.isActive && provider.accounts.length && (
-                    <select
-                        value={activeAccount?.address}
-                        onChange={(e) => provider.setActiveAccount(e.target.value)}
-                        title="Select an account"
-                    >
-                        {provider.accounts.map((account) => (
-                            <option key={account.address} value={account.address}>
-                                {account.address}
-                            </option>
-                        ))}
-                    </select>
-                )}
+              {provider.isActive && provider.accounts.length && (
+                <select
+                  value={activeAccount?.address}
+                  onChange={(e) => provider.setActiveAccount(e.target.value)}
+                  title="Select an account"
+                >
+                  {provider.accounts.map((account) => (
+                    <option key={account.address} value={account.address}>
+                      {account.address}
+                    </option>
+                  ))}
+                </select>
+              )}
             </div>
           </div>
         </div>
