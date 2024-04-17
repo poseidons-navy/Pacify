6553a93f77b6b28378390d1f866ab6b826f3e2b3
mbeka02 <antonymbeka@gmail.com>
2024-04-17 12:23:52 +0300

    fixed breaking changes

--- pacific/src/validation/admin.ts
+++
@@ -1,6 +0,0 @@
-import z from "zod";
-
-export const createAdminSchema = z.object({
-  name: z.string({ required_error: "Please input a name." }).min(2).max(50),
-  walletAddress: z.string({ required_error: "Please input a wallet address." }),
-});
