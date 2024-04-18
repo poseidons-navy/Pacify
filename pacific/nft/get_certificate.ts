f2fdf100b257f4b09ed551b4398b9eab53c8cfd9
mbeka02 <antonymbeka@gmail.com>
2024-04-18 19:58:41 +0300

    feat : added search

--- pacific/nft/get_certificate.ts
+++ pacific/nft/get_certificate.ts
@@ -1,5 +1,6 @@
 import { client as algodClient } from "@/lib/algodClient";
 import { getIndexFromDb } from "@/db/getions";
+
 export async function getNft(assetIndex: number) {
   const assetInfo = await algodClient.getAssetByID(assetIndex).do();
   return assetInfo;
@@ -9,7 +10,7 @@ export async function getNft(assetIndex: number) {
  * @param serial_no String
  */
 export async function getCertificate(serial_no: string) {
-  console.log("Serial Number", serial_no)
+  console.log("Serial Number", serial_no);
   const asset_index = await getIndexFromDb(serial_no);
 
   if (asset_index === undefined || asset_index === null) {
@@ -17,7 +18,7 @@ export async function getCertificate(serial_no: string) {
   }
 
   console.log("Asset Index", asset_index);
-  const cert_nft = getNft(asset_index);
+  const cert_nft = await getNft(asset_index);
   console.log("Certificate NFT", cert_nft);
   return cert_nft;
 }
